4fcc3c5fd500b9f9effff0263737fcda1e9adea7
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-01-31 02:05:32 +0900

    `make test`コマンド 使い方の説明の追加

--- tests/dummy_test.c
+++ tests/dummy_test.c
@@ -7,5 +7,11 @@ int main()
 	// return_one_funcが　一を返すかどうかのテスト 
 	assert(return_one_func() == 1);
 	printf("success!\n");
+	// testsの説明
+	printf(
+		"make testの使い方\n"
+		"make test TEST_FILE=<PATH/TO/TEST>\n"
+		"<PATH/TO/TEST>は、testsディレクトリ内の任意のファイルを指定する\n"
+	);
 	return (0);
 }
