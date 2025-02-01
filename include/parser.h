78efd64b3bee9e51e9d9a2d0aed2d2227940ac3a
Kanta Ara <kaara@c6r3s1.42tokyo.jp>
2025-02-01 18:32:59 +0900

    [wip]構造体の追加

---
+++ include/parser.h
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef enum    e_operator t_operator;
+
+enum    e_operator
+{
+	e_ope_none,
+	e_ope_redirect_i, // <
+	e_ope_redirect_o, // >
+	e_ope_heredoc_i,  // <<
+	e_ope_heredoc_o,  // >>
+	e_ope_and,        // &&
+	e_ope_or          // || 
+};
+
+typedef struct s_ast t_ast;
+
+struct  s_ast
+{
+	t_ast       *left_ast;
+	t_ast       *right_ast;
+	t_operator  ope;
+	char        *cmd;
+	char        *argv;
+};
+
+t_ast  *parse_recursive(t_ast *ast, char *input);
+t_ast  *allocation_ast(void);
+void    check_operator(t_ast *ast, char *input);
