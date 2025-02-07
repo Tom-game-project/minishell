6f7caa101c4a3eeee8bff8a31be41eda7e9998f4
kaara <arakan0613@icloud.com>
2025-02-07 11:36:15 +0900

    [wip]add tinyshell

--- src/exec/subshell.c
+++ src/exec/subshell.c
@@ -10,9 +10,22 @@ int subshell(char *input, char *envp[])
 	t_ast	*ast;
 	int     exit_status;
 
+	if (input == NULL)
+		return (0);
 	parser(&ast, input);
-	if (check_update_arg(ast))
-		exit_status = exec(ast, envp);
-	exit_status = subshell(ast->cmd, envp);
+
+	tinyshell(ast, envp)
+	{
+
+		if (check_update_arg(ast))
+			exit_status = exec(ast, envp);
+		exit_status = subshell(ast->cmd, envp);
+		if (check_logical_operator(ast->right_ast))
+			(ast->right_ast);
+	}
+
 	return (exit_status);
 }
+
+//pipexと違って>が存在しない場合stdoutにpipe内のものを最後に出力する必要がある。
+//andとor演算子を見て処理するか確認する関数が必要
