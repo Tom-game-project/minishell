81618f454a69772f867838129b468680c808e715
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-02-07 12:16:26 +0900

    [WIP] build in関数を定義するファイルにコメントを追加

--- src/built-in/export.c
+++ src/built-in/export.c
@@ -1,3 +1,27 @@
 /// exportは子プロセスを読んでいるようにみえて、親で実行する
 ///
 /// 保持しているenvpのディクショナリ構造を、リスト構造に直してもう一度envpに渡す
+///
+///
+/// 与えられた引数で、exportを試みられたものの環境変数をすべて設定する
+///
+/// ```bash
+/// ❱ export hello=hello
+/// ❱ export hello
+/// ❱ echo $hello
+/// hello
+/// ```
+///
+///
+/// ```bash
+/// export hello=hello world=world
+/// ```
+///
+/// 環境変数に使えない文字が入る場合は
+/// 以下のようなエラーが出力される
+/// exit status は1を返却する
+/// ```
+/// ❱ export =hello
+/// bash: export: `=hello': not a valid identifier
+/// ```
+
