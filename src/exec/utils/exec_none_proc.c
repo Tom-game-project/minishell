61faeae457c4e2dd487357f04146afb34e36f913
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-02-18 12:53:31 +0900

    [WIP] `exec_none_proc`内の関数をリファクタリング

--- src/exec/utils/exec_none_proc.c
+++ src/exec/utils/exec_none_proc.c
@@ -1,17 +1,49 @@
 #include "dict.h"
+#include "list.h"
 #include "parser.h"
 #include "exec.h"
 #include "utils.h"
 
 #include <sys/wait.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+/// TODO:
+/// リファクタリングをするときは、
+/// 子プロセスと親プロセスで分ける
+
+int child_proc_none(int pipe_fd[2],t_exec_args *args)
+{
+	if (args->input_fd != STDIN_FILENO)
+	{
+		dup2(args->input_fd, STDIN_FILENO);
+		close(args->input_fd);
+	}
+	close(pipe_fd[PIPE_READ]);
+	dup2(pipe_fd[PIPE_WRITE], STDOUT_FILENO);
+	close(pipe_fd[PIPE_WRITE]);
+	execve_wrap(args);
+	return(1);
+}
+
+/// 返り値はexit_status
+int parent_proc_none(int pipe_fd[2], int pid)
+{
+	int status;
+
+	close(pipe_fd[PIPE_WRITE]);// 子プロセスに伝えることはない						
+	fd_write(pipe_fd[PIPE_READ]);
+	waitpid(pid, &status, WUNTRACED);
+	close(pipe_fd[PIPE_READ]);
+	return (WEXITSTATUS(status));
+}
+
 
 /// 実行可能な状態であり、かつ、
 /// 自分自身が子プロセス中に入っていない場合(親プロセスから)実行される関数
 int none_proc(t_exec_args *args)
 {
 	int pid;
-	int status;
 	int pipe_fd[2];
 
 	if (pipe(pipe_fd) == -1)
@@ -19,24 +51,9 @@ int none_proc(t_exec_args *args)
 	pid = fork();
 	if (pid == 0)
 	{
-		if (args->input_fd != STDIN_FILENO)
-		{
-			dup2(args->input_fd, STDIN_FILENO);
-			close(args->input_fd);
-		}
-		close(pipe_fd[PIPE_READ]);
-		dup2(pipe_fd[PIPE_WRITE], STDOUT_FILENO);
-		close(pipe_fd[PIPE_WRITE]);
-		execve_wrap(args);
-		return(1);
+		child_proc_none(pipe_fd, args);
 	}
-	else
-	{
-		close(pipe_fd[PIPE_WRITE]);// 子プロセスに伝えることはない						
-		fd_write(pipe_fd[PIPE_READ]);
-		waitpid(pid, &status, WUNTRACED);
-		close(pipe_fd[PIPE_READ]);
-		return (WEXITSTATUS(status));
-	}
+	str_list_clear(&(args->args), free);
+	return (parent_proc_none(pipe_fd, pid));
 }
 
