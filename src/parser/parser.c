8b4f1bcdefcf6687fb40da44ba2c474f0d62ff31
Kanta Ara <kaara@c6r4s3.42tokyo.jp>
2025-03-06 19:08:59 +0900

    [wip]parserをnorm仕様で分割

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -3,7 +3,7 @@
 #include "parser.h"
 #include <stdlib.h>
 
-void get_next_input(t_ast **ast, t_str_list	**next_input, char **str);
+t_parse_result get_next_input(t_ast **ast, t_str_list	**next_input, char **str);
 
 t_parse_result	parser(t_ast **ast, char *input)
 {
@@ -19,13 +19,9 @@ t_parse_result	parser(t_ast **ast, char *input)
 		return (e_result_paren_not_closed_err);
 	str = ft_strdup(input);
 	*ast = allocation_ast();
-	get_next_input(ast, &next_input, &str);
+	result = get_next_input(ast, &next_input, &str);
 	if (next_input == NULL)
-	{
-		result = separate_and_store_cmd_args(*ast, &str);
-		free(str);
 		return (result);
-	}
 	head = next_input;
 	result = parser(&((*ast)->left_ast), head->ptr.str);
 	if (result == e_result_ok && next_input->next != NULL)
@@ -38,12 +34,22 @@ t_parse_result	parser(t_ast **ast, char *input)
 	return (result);
 }
 
-void get_next_input(t_ast **ast, t_str_list	**next_input, char **str)
+t_parse_result get_next_input(t_ast **ast, t_str_list	**next_input, char **str)
 {
+	t_parse_result result;
+
+	result = e_result_ok;
 	if (checker_str_ctl(*str))
 		*next_input = separate_and_store_control_operators(*ast, str);
 	else if (checker_str_pipe(*str))
 		*next_input = separate_and_store_pipe_operators(*ast, str);
 	else if (checker_str_rdt(*str))
 		*next_input = separate_and_store_redirect_operators(*ast, str);
+	else
+	{
+		result = separate_and_store_cmd_args(*ast, str);
+		free(*str);
+		return (result);
+	}
+	return (result);
 }
