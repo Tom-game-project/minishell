56b6e7436e689e54e96c90c1b1760ab4eff3f796
Kanta Ara <kaara@c6r4s3.42tokyo.jp>
2025-03-07 17:40:28 +0900

    asterisk用のdirを追加

---
+++ src/exec/asterisk/expand_asterisk.c
@@ -0,0 +1,11 @@
+#include "expand_string.h"
+#include "list.h"
+#include "parser.h"
+
+t_str_list  *expand_asterisk(t_str_list *cmd)
+{
+    t_str_list  *after_expand;
+
+    after_expand = NULL;
+    return (after_expand);
+}
