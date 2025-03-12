78476b3513ce57707d685202fb6d1b9488b3e318
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-03-13 02:03:09 +0900

    [WIP] stderrにエラーを出力できるようにする

--- src/ast_checker/print_checker_result.c
+++ src/ast_checker/print_checker_result.c
@@ -1,6 +1,7 @@
 #include "ast_checker.h"
 #include "test_tools.h"
 #include <unistd.h>
+#include "libft.h"
 
 /// TODO 書き換える！！！
 bool    print_checker_result(t_syntax_result result)
@@ -8,16 +9,16 @@ bool    print_checker_result(t_syntax_result result)
     if (result == e_syntax_ok)
         return (true);
     else if (result == e_ctl_near_unexpected_token_and)
-        debug_dprintf(STDERR_FILENO, "minishell : near_unexpected_token_and\n");
+        ft_putstr_fd("minishell : near_unexpected_token_and\n", STDERR_FILENO);
     else if (result == e_ctl_near_unexpected_token_or)
-        debug_dprintf(STDERR_FILENO,"minishell : near_unexpected_token_or\n");
+        ft_putstr_fd("minishell : near_unexpected_token_or\n", STDERR_FILENO);
     else if (result == e_rdt_near_unexpected_token_pipe)
-        debug_dprintf(STDERR_FILENO,"minishell : near_unexpected_token_pipe\n");
+        ft_putstr_fd("minishell : near_unexpected_token_pipe\n", STDERR_FILENO);
     else if (result == e_rdt_near_unexpected_token_newline)
-        debug_dprintf(STDERR_FILENO, "minishell : near_unexpected_token_newline\n");
+        ft_putstr_fd( "minishell : near_unexpected_token_newline\n", STDERR_FILENO);
     else if (result == e_rdt_near_unexpected_token_rdt)
-        debug_dprintf(STDERR_FILENO, "minishell : near_unexpected_token_rdt\n");
+        ft_putstr_fd( "minishell : near_unexpected_token_rdt\n", STDERR_FILENO);
     else if (result == e_syntax_near_unexpected_token_string)
-        debug_dprintf(STDERR_FILENO, "minishell : near_unexpected_token_string\n");
+        ft_putstr_fd( "minishell : near_unexpected_token_string\n", STDERR_FILENO);
     return (false);
 }
