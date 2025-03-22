ab39ba02b157c99cf8293891e4e9917c6e50e39b
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-03-22 18:45:20 +0900

    [WIP] normの修正

--- src/list/candy_cutter.c
+++ src/list/candy_cutter.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   candy_cutter.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmuranak <tmuranak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 18:20:20 by tmuranak          #+#    #+#             */
+/*   Updated: 2025/03/22 18:20:35 by tmuranak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "list.h"
 #include <stdlib.h>
 
 /// 改行がある部分までで切り取る関数
-char *candy_cutter(t_str_list **lst, int index)
+char	*candy_cutter(t_str_list **lst, int index)
 {
-	t_str_list *t;
-	char *str;
+	t_str_list	*t;
+	char		*str;
 
 	t = str_list_cut(lst, index);
 	str = str_list_join(t, "");
