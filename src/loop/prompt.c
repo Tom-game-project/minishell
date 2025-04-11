402c6e7ca4a6052b2867bfd56dfea60112b0911e
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-04-11 16:56:51 +0900

    [WIP] `exit_status`に色をつけた

--- src/loop/prompt.c
+++ src/loop/prompt.c
@@ -4,25 +4,36 @@
 #include <stdlib.h>
 #include <limits.h>
 
+
+static void error_color(t_char_list *lst, int exit_status)
+{
+	char *exit_str;
+
+	exit_str = ft_itoa(exit_status);
+	if (exit_status == 0)
+		char_list_push_str(&lst, "\e[32m [");
+	else 
+		char_list_push_str(&lst, "\e[31m [");
+	char_list_push_str(&lst, exit_str);
+	char_list_push_str(&lst, "]\e[m");
+	free(exit_str);
+}
+
+
 // prompt
 //
 char	*prompt(int exit_status)
 {
 	char		buf[PATH_MAX];
-	char		*exit_str;
 	t_char_list	*rstr_list;
 	char		*rstr;
 
-	exit_str = ft_itoa(exit_status);
 	rstr_list = NULL;
 	if (getcwd(buf, PATH_MAX) != NULL)
 		char_list_push_str(&rstr_list, buf);
-	char_list_push_str(&rstr_list, " [");
-	char_list_push_str(&rstr_list, exit_str);
-	char_list_push(&rstr_list, ']');
+	error_color(rstr_list, exit_status);
 	char_list_push_str(&rstr_list, "> ");
 	rstr = char_list_to_str(rstr_list);
 	char_list_clear(&rstr_list);
-	free(exit_str);
 	return (rstr);
 }
